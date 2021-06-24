e61f50d50a97446818b046f25c7258bad7523c1b
Paweł G <pa.gny@wp.pl>
2021-06-24 20:31:26 +0200

    change 3 lines

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
 num1 = float(input('Enter the first number: '))
-information = input("Select an operator from the list +,-,*,/: ")
+information = input("Select an operator from the list (+,-,*,/) : ")
 num2 = float(input('Enter the second number: '))
 if information == '+':
     print(num1 + num2)
